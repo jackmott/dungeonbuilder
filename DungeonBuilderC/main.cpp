44a70197e3d5d3338ad24e2d7cba5eb5e62e243d
jackmott <jack.mott@gmail.com>
2016-06-04 00:02:38 -0500

    block set_escdelay from windows builds

--- DungeonBuilderC/main.cpp
+++ DungeonBuilderC/main.cpp
@@ -13,7 +13,12 @@ void curses_init()
 	cbreak();
 	keypad(stdscr,true);
 	start_color();
+
+//This function only exists in ncurses, not pdcurses
+//and is necessary or there is a long delay on ESC key
+#ifndef _WIN32
 	set_escdelay(25);
+#endif
 }
 
 int main(int argc,char* argv[])
